91bd27c78e13723f1fa405e1b5c8e5daf439b84d
Jonas Rosland <jonas.rosland@gmail.com>
2025-09-08 16:55:09 -0400

    Add support for NVIDIA GPUs

--- run-gpu.sh
+++ run-gpu.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 # GPU-enabled run script for Windows/WSL
 
-docker run --rm --gpus all \
-  -e NVIDIA_VISIBLE_DEVICES=all \
-  -e NVIDIA_DRIVER_CAPABILITIES=compute,utility \
-  -v "$(pwd)/data:/data" \
-  automatic-transcriptions-gpu "$@"
+# Check if first argument is --entrypoint
+if [ "$1" = "--entrypoint" ]; then
+  # Handle --entrypoint case
+  shift  # Remove --entrypoint
+  ENTRYPOINT="$1"
+  shift  # Remove the entrypoint command
+  docker run --rm --gpus all \
+    -e NVIDIA_VISIBLE_DEVICES=all \
+    -e NVIDIA_DRIVER_CAPABILITIES=compute,utility \
+    -v "$(pwd)/data:/data" \
+    --entrypoint "$ENTRYPOINT" \
+    automatic-transcriptions-gpu "$@"
+else
+  # Normal case
+  docker run --rm --gpus all \
+    -e NVIDIA_VISIBLE_DEVICES=all \
+    -e NVIDIA_DRIVER_CAPABILITIES=compute,utility \
+    -v "$(pwd)/data:/data" \
+    automatic-transcriptions-gpu "$@"
+fi
